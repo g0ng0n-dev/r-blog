70ef15047f5f70ebb492f50811d910485328183e
Gon Gis <g0ng0n.dev@gmail.com>
2021-05-31 13:51:00 +0200

    fixing small issue on reducer

--- src/redux/reducers/postsReducer.js
+++ src/redux/reducers/postsReducer.js
@@ -5,9 +5,6 @@ export default function postsReducer(state = initialState, action) {
   switch (action.type) {
     case types.LOAD_POSTS_SUCCESS:
       return [...state.posts, ...action.posts];
-    case types.FILTER_POSTS_BY_USER_ID_SUCCESS:
-      console.log("FILTER_POSTS_BY_USER_ID_SUCCESS");
-      return state.filter((post) => post.userId === parseInt(action.id));
     default:
       return state;
   }
